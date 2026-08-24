948b3f461d38c490c4cfc57e0dff9fcf00cd66ea
Natalia Sekula (Ext) <ext.natalia.sekula@hiab.com>
2026-08-24 21:14:17 +0200

    feat: upload xlsx file mechanism

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import customtkinter as ctk
+from customtkinter import filedialog
 import threading
 from PIL import Image
-
+import os
+import shutil
 
 
 class commodity_code_updater_app(ctk.CTk):
@@ -11,17 +13,17 @@ class commodity_code_updater_app(ctk.CTk):
         # store status
         self.sap_task = main_sap_task
         self.desc_task = main_desc_task
-
+        self.selected_file_path = None
 
         # app window setup
-        self.geometry("450x250") 
+        self.geometry("450x480") 
         self.resizable(False, False)
 
         #display
         ctk.set_appearance_mode("System")
         ctk.set_default_color_theme("blue")
 
-        # upper label
+        # upper panel
         self.iconbitmap("static/hiab.ico")
         self.title("commodity code updater")
 
@@ -30,25 +32,103 @@ class commodity_code_updater_app(ctk.CTk):
         self.logo_label = ctk.CTkLabel(self, image=self.logo, text="")
         self.logo_label.pack(pady=(30, 20))
 
-        #title
-        # self.title_label = ctk.CTkLabel(self, text="zmmmzmz", font=("Helvetica", 24, "bold"))
-        # self.title_label.pack(pady=(30, 20))
-
+        #BUTTONS 
 
         # sap button
-        self.sap_button = ctk.CTkButton(self, text="Start SAP Automation", command=self.sap_button_clicked,
+        self.sap_button = ctk.CTkButton(self, text="Start SAP Automation", command= lambda: self.button_clicked(self.sap_task),
                                      width=250, height=50, font=("Helvetica", 16, "bold"), 
                                      fg_color="#4CAF50", hover_color="#45a049")
         self.sap_button.pack(pady=10)
 
         # desc button
-        self.desc_button = ctk.CTkButton(self, text="Start Description Scraping Automation", command=self.desc_button_clicked,
+        self.desc_button = ctk.CTkButton(self, text="Start Description Scraping Automation", command= lambda: self.button_clicked(self.desc_task),
                                      width=250, height=50, font=("Helvetica", 16, "bold"), 
                                      fg_color="#4CAF50", hover_color="#45a049")
         self.desc_button.pack(pady=10)
 
+        # update data button
+        self.update_data_button = ctk.CTkButton(self, text="Start Update Data", 
+                                                command=lambda: self.open_file_popup(self.desc_task),
+                                                width=250, height=50, font=("Helvetica", 16, "bold"), 
+                                                fg_color="#4CAF50", hover_color="#45a049")
+        self.update_data_button.pack(pady=10)
+
+        
+
         self.status_label = ctk.CTkLabel(self, text="Status: Ready to start.", font=("Helvetica", 14))
         self.status_label.pack(pady=10)
+
+
+        # input file frame
+        self.file_frame = ctk.CTkFrame(self, fg_color="transparent")
+        self.file_frame.pack(pady=10)
+
+
+
+    def open_file_popup(self, task_to_run):
+
+
+
+            popup = ctk.CTkToplevel(self)
+            popup.geometry("400x200")
+            popup.resizable(False, False)
+            # upper panel
+            popup.iconbitmap("static/hiab.ico")
+            popup.title("commodity code updater")
+            
+            #lock main window while popup is open
+            popup.transient(self)
+            popup.grab_set()
+            
+            # reset previous selection
+            self.selected_file_path = None
+
+
+            ctk.CTkLabel(popup, text="Upload Nomenclature EN.xlsx if you want to update the data", font=("Helvetica", 14)).pack(pady=(20, 5))
+            
+            file_label = ctk.CTkLabel(popup, text="No file selected...", font=("Helvetica", 12))
+            file_label.pack(pady=5)
+
+            def browse():
+                file_path = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx")])
+                if file_path:
+                    self.selected_file_path = file_path
+                    file_label.configure(text=os.path.basename(file_path))
+
+            
+
+
+            def apply():
+                if not self.selected_file_path:
+                    file_label.configure(text="Please select a file first!", text_color="red")
+                    return
+
+                # import file to data folder
+                target_folder = "data"
+                target_filename = "Nomenclature EN.xlsx"
+                target_path = os.path.join(target_folder, target_filename)
+
+                try:
+                    if os.path.exists(target_path):
+                        os.remove(target_path)
+
+                    shutil.copy(self.selected_file_path, target_path)
+                    
+                    popup.destroy()
+
+                except Exception as e:
+                    file_label.configure(text="Error copying file!", text_color="red")
+                    print(f"File Error: {e}")
+
+
+            # BUTTONS
+            ctk.CTkButton(popup, text="Browse", command=browse, width=100).pack(side = "left", padx=20)
+
+            ctk.CTkButton(popup, text="Apply", command=apply, width=100).pack(side="left", padx=10)
+
+            ctk.CTkButton(popup, text="Run", command=lambda: task_to_run, width=100).pack(side="left", padx=10)
+
+
 
     # threading logic (connect with main without freezing ui )
     def run_thread(self, task):
@@ -62,20 +142,16 @@ class commodity_code_updater_app(ctk.CTk):
             self.status_label.configure(text="Status: Process failed with errors.")
         finally:
             self.sap_button.configure(state="normal")
+            self.desc_button.configure(state="normal")
+            self.update_data_button.configure(state="normal")
 
     #start -> thread trigger
-    def sap_button_clicked(self):
+    def button_clicked(self, task):
         self.sap_button.configure(state="disabled")
-        self.status_label.configure(text="Status: Automation running...")
-
-        threading.Thread(target=self.run_thread, args=(self.sap_task,)).start()
-        
-
-
-    def desc_button_clicked(self):
         self.desc_button.configure(state="disabled")
+        self.update_data_button.configure(state="disabled")
+
         self.status_label.configure(text="Status: Automation running...")
 
-        threading.Thread(target=self.run_thread, args=(self.desc_task,)).start()
-
-# if desc_button_clicked the other main.py (eg main2) logic will start .how to do it and it is a good idea?
+        threading.Thread(target=self.run_thread, args=(task,)).start()
+        
--- main.py
+++ main.py
@@ -39,119 +39,67 @@ def main_desc():
             print("web error\n")
             return
         spreadsheet_handler.update_google_sheets2(row, sheet)
-        
-
-
+   
 
 if __name__ == "__main__":
     ui = app.commodity_code_updater_app(main_sap_task=main_sap, main_desc_task=main_desc)
     ui.mainloop()
 
-    # commodity_col_index = 7 
- 
-    # print("final HS CODE: ", group_df['final HS CODE'])
-    # print("connect to SAP\n")
-    # try:
-    #     SapGuiAuto = win32com.client.GetObject("SAPGUI")
-    #     application = SapGuiAuto.GetScriptingEngine
-    #     connection = application.Children(0)
-    #     session = connection.Children(0)
-    # except Exception as e:
-    #     print(e)
-    #     return
-
-    # session.findById("wnd[0]/tbar[0]/okcd").text = "ZMM_UPDATE2"
-    # session.findById("wnd[0]/tbar[0]/btn[0]").press() 
-
-    # try:
-    #     for hs_code, group_data in group_df:
-    #         print("updating HS Code: ", hs_code)
-    #         print("batch button\n")
-    #         session.findById("wnd[0]/usr/btn%_S_MATNR_%_APP_%-VALU_PUSH").press()
-
-
-    #         group_data['Material'].to_clipboard(index=False, header=False)
-
-    #         clipboard = pyperclip.paste()
-    #         print("clipboard: ", clipboard)
-    #        # time.sleep(1)
-
-
-    #         # clear button in multiple selection
-    #         print("clear button\n")
-    #         session.findById("wnd[1]/tbar[0]/btn[16]").press()
-        
-    #         # uplad from clickboard
-    #         print("upload from clipboard\n")
-    #         session.findById("wnd[1]/tbar[0]/btn[24]").press()
-    #         #time.sleep(1)
-
-    #         # exit from clickboard
-    #         print("exit from clipboard\n")
-    #         session.findById("wnd[1]/tbar[0]/btn[8]").press()
-    #        # time.sleep(1)
-
-    #         # choose plant
-    #         print("choose plant\n")
-    #         session.findById("wnd[0]/usr/ctxtS_WERKS-LOW").text = "FI63"
-    #         session.findById("wnd[0]/usr/chkC_PLANT").selected = True
-
-    #         # open execute
-    #         print("run execute\n")
-    #         session.findById("wnd[0]/tbar[1]/btn[8]").press()
-    #         time.sleep(3)
-
-    #         #catch  status bar    
-    #         print("catch status bar\n")
-    #         status_bar = session.findById("wnd[0]/sbar")
-    #         print("status bar: ", status_bar.text)
-    #         if ( status_bar.text == "No data found with given inputs"):
-    #             print("no data found with given inputs\n")
-    #             continue
-
-    #         #select all
-    #         print("select all\n")
-    #         session.findById("wnd[0]/tbar[1]/btn[13]").press()
-    #         time.sleep(1)
-
-    #         #choose commodity code
-    #         print("choose commodity code\n")
-    #         session.findById("wnd[0]/usr/tabsMAT_MASTER/tabpMAT_MASTER_FC3/ssubMAT_MASTER_SCA:ZMMR_MATERIAL_MASTER_UPD2:0103/cmbZPLANT_LIST_DROPDOWN").key = "CCOD"
-    #         time.sleep(1)
-    #         session.findById("wnd[0]/usr/tabsMAT_MASTER/tabpMAT_MASTER_FC3/ssubMAT_MASTER_SCA:ZMMR_MATERIAL_MASTER_UPD2:0103/ctxtZPLANT_VALUE").text = hs_code
-    #         time.sleep(1)
-
-    #         #mass change
-    #         print("mass change\n")
-    #         session.findById("wnd[0]/usr/tabsMAT_MASTER/tabpMAT_MASTER_FC3/ssubMAT_MASTER_SCA:ZMMR_MATERIAL_MASTER_UPD2:0103/btnMASSCHANGE").press()
-
-    #         #update
-    #         print("update\n")
-    #         session.findById("wnd[0]/tbar[1]/btn[29]").press()
-
-    #         #back
-    #         print("back from execute\n")
-    #         session.findById("wnd[0]/tbar[0]/btn[3]").press()
-
-
-    #         print("updating Google Sheets")
-    #         cells_to_update = []
-            
-
-    #         for index, row_data in group_data.iterrows():
-    #             sheet_row = row_data['sheet_row']
-                
-    #             cells_to_update.append(gspread.Cell(row=sheet_row, col=commodity_col_index, value=hs_code))
-            
-    #         # push changes
-    #         sheet.update_cells(cells_to_update, value_input_option='USER_ENTERED')
-    #         print(f"sheets updated: {len(cells_to_update)} rows changed to {hs_code}\n")
 
 
 
+# def main_update_data():
+    
+#     df = web_handler.scrap(df)
+#     now = datetime.now()
+    
+#     page.goto("https://circabc.europa.eu/ui/group/0e5f18c2-4b2f-42e9-aed4-dfe50ae1263b/library/566dd333-1deb-4235-982a-4fdeaf3657c1?p=1&n=-1&sort=name_ASC")
+    
+#     current_month_str = now.strftime("%m - %B") 
+#     dynamic_pattern = re.compile(f"^{current_month_str}$")
 
-    # except Exception as e:
-    #     print("error")
-    #     return
+#     with sync_playwright() as p:
+#         browser = p.chromium.launch(headless=False)
+#         context = browser.new_context()
+#         page = context.new_page()
+
+#         page.get_by_role("table").locator("a").filter(has_text=dynamic_pattern).click()
+
+#         page.get_by_role("link", name="Nomenclature EN.xlsx").click()
+
+#         with page.expect_download() as download3_info:
+#             page.get_by_text("Download", exact=True).click()
+#             download3 = download3_info.value
+#             download3.save_as("Nomenclature EN.xlsx")
 
 
+
+     
+# import re
+# from datetime import datetime
+# from playwright.sync_api import sync_playwright
+# import pandas as pd
+
+# def main_download_data():
+    
+#     df = web_handler.scrap(df)
+#     now = datetime.now()
+    
+#     page.goto("https://circabc.europa.eu/ui/group/0e5f18c2-4b2f-42e9-aed4-dfe50ae1263b/library/566dd333-1deb-4235-982a-4fdeaf3657c1?p=1&n=-1&sort=name_ASC")
+    
+#     current_month_str = now.strftime("%m - %B") 
+#     dynamic_pattern = re.compile(f"^{current_month_str}$")
+
+#     with sync_playwright() as p:
+#         browser = p.chromium.launch(headless=False)
+#         context = browser.new_context()
+#         page = context.new_page()
+
+#         page.get_by_role("table").locator("a").filter(has_text=dynamic_pattern).click()
+
+#         page.get_by_role("link", name="Nomenclature EN.xlsx").click()
+
+#         with page.expect_download() as download3_info:
+#             page.get_by_text("Download", exact=True).click()
+#             download3 = download3_info.value
+#             download3.save_as("Nomenclature EN.xlsx")
