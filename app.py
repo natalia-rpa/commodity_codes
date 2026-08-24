9b86a60f52113ac9955cdec469b1139af33982b0
Natalia Sekula (Ext) <ext.natalia.sekula@hiab.com>
2026-08-24 07:33:34 +0200

    create ui

--- app.py
+++ app.py
@@ -5,23 +5,23 @@ from PIL import Image
 
 
 class commodity_code_updater_app(ctk.CTk):
-    def __init__(self, main):
+    def __init__(self, main_sap_task, main_desc_task):
         super().__init__()
         
         # store status
-        self.automation_task = main
-        
-        # app window setup
+        self.sap_task = main_sap_task
+        self.desc_task = main_desc_task
 
-        
-        
+
+        # app window setup
         self.geometry("450x250") 
         self.resizable(False, False)
 
+        #display
         ctk.set_appearance_mode("System")
         ctk.set_default_color_theme("blue")
 
-        # build the ui
+        # upper label
         self.iconbitmap("static/hiab.ico")
         self.title("commodity code updater")
 
@@ -30,34 +30,52 @@ class commodity_code_updater_app(ctk.CTk):
         self.logo_label = ctk.CTkLabel(self, image=self.logo, text="")
         self.logo_label.pack(pady=(30, 20))
 
+        #title
         # self.title_label = ctk.CTkLabel(self, text="zmmmzmz", font=("Helvetica", 24, "bold"))
         # self.title_label.pack(pady=(30, 20))
 
-        self.start_button = ctk.CTkButton(self, text="Start Automation", command=self.start_clicked,
+
+        # sap button
+        self.sap_button = ctk.CTkButton(self, text="Start SAP Automation", command=self.sap_button_clicked,
                                      width=250, height=50, font=("Helvetica", 16, "bold"), 
                                      fg_color="#4CAF50", hover_color="#45a049")
-        self.start_button.pack(pady=10)
+        self.sap_button.pack(pady=10)
+
+        # desc button
+        self.desc_button = ctk.CTkButton(self, text="Start Description Scraping Automation", command=self.desc_button_clicked,
+                                     width=250, height=50, font=("Helvetica", 16, "bold"), 
+                                     fg_color="#4CAF50", hover_color="#45a049")
+        self.desc_button.pack(pady=10)
 
         self.status_label = ctk.CTkLabel(self, text="Status: Ready to start.", font=("Helvetica", 14))
         self.status_label.pack(pady=10)
 
-    # threading logic
-    def run_thread(self):
-        """Runs the injected workflow without freezing the UI"""
+    # threading logic (connect with main without freezing ui )
+    def run_thread(self, task):
         try:
-            if self.automation_task:
-                self.automation_task() # Execute the stored function
+            if task:
+                task() # Execute the stored function
                 
             self.status_label.configure(text="Status: Finished successfully!")
         except Exception as e:
             print(f"CRITICAL ERROR: {str(e)}")
             self.status_label.configure(text="Status: Process failed with errors.")
         finally:
-            self.start_button.configure(state="normal")
+            self.sap_button.configure(state="normal")
 
-    def start_clicked(self):
-        """Triggered when the green Start button is pressed"""
-        self.start_button.configure(state="disabled")
+    #start -> thread trigger
+    def sap_button_clicked(self):
+        self.sap_button.configure(state="disabled")
         self.status_label.configure(text="Status: Automation running...")
+
+        threading.Thread(target=self.run_thread, args=(self.sap_task,)).start()
         
-        threading.Thread(target=self.run_thread).start()
+
+
+    def desc_button_clicked(self):
+        self.desc_button.configure(state="disabled")
+        self.status_label.configure(text="Status: Automation running...")
+
+        threading.Thread(target=self.run_thread, args=(self.desc_task,)).start()
+
+# if desc_button_clicked the other main.py (eg main2) logic will start .how to do it and it is a good idea?
--- main.py
+++ main.py
@@ -1,21 +1,50 @@
+from sqlite3.dbapi2 import Row
 import spreadsheet_handler
 import sap_handler 
 import app
+import web_handler
 
+def main_sap():
+    sheet_name = "data2"
+    df, sheet = spreadsheet_handler.connect_to_google_sheets(sheet_name)
+    index_to_update = 7
+    
+    # diff  Commodity_Code final HS CODE
+    diff_df = df[df['Commodity_Code'] != df['final HS CODE']]
 
-def main():
-    group_df, sheet = spreadsheet_handler.connect_to_google_sheets()
+    # group by final HS CODE
+    group_df = diff_df.groupby('final HS CODE')
 
     session = sap_handler.init_sap()
-    
+
     print("connected to SAP\n")
     for _, group_data in group_df:
-        sap_handler.update_sap(session, group_data)
+        if not sap_handler.update_sap(session, group_data):
+            print("sap error\n")
+            return
         spreadsheet_handler.update_google_sheets(group_data, sheet)
 
 
+def main_desc():
+    sheet_name = "guide"
+    index_to_update = 7
+
+    df, sheet = spreadsheet_handler.connect_to_google_sheets(sheet_name)
+
+    df = web_handler.scrap(df)
+
+
+    for row in df:
+        if not web_handler.scrap(row):
+            print("web error\n")
+            return
+        spreadsheet_handler.update_google_sheets2(row, sheet)
+        
+
+
+
 if __name__ == "__main__":
-    ui = app.commodity_code_updater_app(main)
+    ui = app.commodity_code_updater_app(main_sap_task=main_sap, main_desc_task=main_desc)
     ui.mainloop()
 
     # commodity_col_index = 7 
